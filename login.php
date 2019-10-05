4a52d415c6edd9d73795217b63c1bf1e0a012ee3
Gabriel <gibosco@gmail.com>
2019-10-05 16:15:22 -0300

    Agregado el control de existencia de usuario y usuario activado

--- login.php
+++ login.php
@@ -4,12 +4,12 @@
     
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $access = "1";
+    //$access = "1";
     
     if($link){
-      $statement = mysqli_prepare($link, "SELECT * FROM users WHERE username = ? AND password = ? AND access = ?");
+      $statement = mysqli_prepare($link, "SELECT * FROM users WHERE username = ? AND password = ?"); //AND access = ?");
       if($statement){
-        mysqli_stmt_bind_param($statement, "sss", $username, $password, $access);
+        mysqli_stmt_bind_param($statement, "ss", $username, $password); //, $access);
         mysqli_stmt_execute($statement);
     
         mysqli_stmt_store_result($statement);
@@ -19,11 +19,15 @@
        
     $response = array();
     $response["succes"] = false; 
+	$response["exists"] = false;
     
+	//Si hay resultado va a ser único (no permitimos usuarios duplicados)
     while(mysqli_stmt_fetch($statement)){
-        $response["succes"] = true;  
-        $response["name"] = $name;
-        $response["surname"] = $surname;
+		$response["exists"] = true;
+		if($access == '1'){
+			$response["succes"] = true;  
+			$response["name"] = $name;
+			$response["surname"] = $surname;
     }
     
     echo json_encode($response);
