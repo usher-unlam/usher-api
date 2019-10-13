8ecfb06e573a3073ed7c73b74c9399244271ad98
Gabriel <gibosco@gmail.com>
2019-10-13 13:40:53 -0300

    Modificado el array. Ahora usa de índice el id de la fila para no pisar la info

--- block_hist.php
+++ block_hist.php
@@ -10,7 +10,7 @@
 	}
     
     if($link){
-		$statement = mysqli_prepare($link, "SELECT blocks.name, block_history.session_id, block_history.minutes, block_history.presents, block_history.total
+		$statement = mysqli_prepare($link, "SELECT blocks.name, block_history.id, block_history.session_id, block_history.minutes, block_history.presents, block_history.total
 											FROM users, blocks, block_history
 											WHERE users.username LIKE ?
 											AND users.member_id = blocks.head_id
@@ -20,7 +20,7 @@
 		if($statement){		
 			mysqli_stmt_execute($statement);
 			mysqli_stmt_store_result($statement);
-			mysqli_stmt_bind_result($statement, $block_name, $session_id, $minutes, $presents, $total);
+			mysqli_stmt_bind_result($statement, $id, $session_id, $block_name, $minutes, $presents, $total);
         }
     }
        
@@ -28,12 +28,13 @@
     $response["succes"] = false; 
     
     while(mysqli_stmt_fetch($statement)){
-        $response["succes"] = true;  
-        $response[$session_id]["block"] = $block_name;
-        $response[$session_id]["session"] = $session_id;
-        $response[$session_id]["minutes"] = $minutes;
-        $response[$session_id]["presents"] = $presents;
-		$response[$session_id]["total"] = $total;
+        $response["succes"] = true;
+		$response[$id]["session_id"] = $session_id;
+        $response[$id]["block"] = $block_name;
+        $response[$id]["session"] = $session_id;
+        $response[$id]["minutes"] = $minutes;
+        $response[$id]["presents"] = $presents;
+		$response[$id]["total"] = $total;
     }
     
     echo json_encode($response);
