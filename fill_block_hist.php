0f8bb3d37ac721943c5008d2bc11ada0370bc0d7
Servidor Usher <server@usher.sytes.net>
2019-11-02 16:14:42 -0300

    commit desde FTP

--- fill_block_hist.php
+++ fill_block_hist.php
@@ -1,3 +1,5 @@
+//REVISAR TODA LA LÓGICA
+
 <?php
 require_once 'db_connect_web.php';
 
@@ -14,7 +16,8 @@ else{
 if($link){
 		$statement = mysqli_prepare($link, "SELECT camserver, prioridad, tstamp, estadoUbicaciones
     									FROM status
-											WHERE camserver LIKE 'SVR1'");
+											WHERE camserver LIKE 'SVR1'
+                      ORDER BY tstamp DESC");
 		//mysqli_stmt_bind_param($statement, "s", $svr);
       
 		if($statement){		
@@ -25,20 +28,55 @@ if($link){
     else{
        $response["error"] = "La consulta no fue ejecutada";
     }
+    
+    $statement_benchs = mysqli_prepare($link, "SELECT number, associated_member, associated_block FROM benchs");
+      
+    if($statement_benchs){		
+			  mysqli_stmt_execute($statement_benchs);
+			  mysqli_stmt_store_result($statement_benchs);
+			  mysqli_stmt_bind_result($statement_benchs, $number, $associated_member, $associated_block);
+    }
+    else{
+        $response["error"] = "La consulta de bancas no fue ejecutada";
+    }
 }
 else{
    $response["error"] = "No se estableció la conexión a la base de datos";
 }
 
-$benchs["succes"] = false;       
-$response["succes"] = false;
+//$benchs["succes"] = false;       
+//$response["succes"] = false;
 
-$regTotales = 0;
-//Hago un primer fetch para inicializar el array de bancas con los valores del primer registro y para conocer la cantidad de bancas para el for
+//Primero tengo que saber a qué bloque pertenece cada banca. Cargo array de bancas con bloques y miembros asociados
+While(mysqli_stmt_fetch($statement_benchs)){
+  $benchs[$number][$block] = $associated_block;
+  $benchs[$number][$member] = $associated_member;
+}
+
+//Hasta ACA estaría OK
+
+$minutes = 0;
+$regTot= 0;
+//Hago un primer fetch para inicializar el array de bloques con los valores en 0 y para conocer la cantidad de bancas para el for
 mysqli_stmt_fetch($statement);
+$regTot++;
+for($i=1 ; $i<=strlen($estadoUbicaciones); $i++){ 
+    $blocks[$benchs[$i][$block]]["presents"] = 0;
+}
+
+// Luego de inicializado, recorro el primer row de nuevo para sumar los estados presentes de cada bloque.
+// En $benchs[$i][$block] tengo el bloque al cual pertenece la banca correspondiente a la posición del string que estoy leyendo (estado)
 for($i=1 ; $i<=strlen($estadoUbicaciones); $i++){
-    $benchs[$i]["session"] = $session; 
-    $benchs[$i]["presences"] = (int) $estadoUbicaciones[$i-1];
+    $blocks[$benchs[$i][$block]]["session"] = $session; 
+    $blocks[$benchs[$i][$block]]["presents"] += $estadoUbicaciones[$i-1];
+}
+
+// Sumo las precencias 1correspondientes a los 
+While(mysqli_stmt_fetch($statement)){
+  for($i=1 ; $i<=strlen($estadoUbicaciones); $i++){
+    $blocks[$benchs[$i][$block]]["session"] = $session; 
+    $blocks[$benchs[$i][$block]]["presents"] += $estadoUbicaciones[$i-1];
+  }
 }
 
 //Recorro las rows y para cada una recorro el string estadoUbicaciones y sumo (0 o 1) a la cantidad de presencias de cada banca ($benchs[$i]) 
